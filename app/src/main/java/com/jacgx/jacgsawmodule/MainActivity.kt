7c8472e3520616fc0b91a9aff46412c419ecdd17
alex <alex@jacgx.com>
2022-12-26 08:49:14 -0500

    fix3

--- app/src/main/java/com/jacgx/jacgsawmodule/MainActivity.kt
+++ app/src/main/java/com/jacgx/jacgsawmodule/MainActivity.kt
@@ -3,19 +3,19 @@ package com.jacgx.jacgsawmodule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.jacgx.corejac.CoreV1
+//import com.jacgx.corejac.CoreV1
 import com.jacgx.jaccore.Corev
 
 class MainActivity : AppCompatActivity() {
 
     private val core = Corev()
-    private val coreV1 = CoreV1()
+    //private val coreV1 = CoreV1()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("developer",core.getCore())
-        Log.d("developer",coreV1.getJac())
+        //Log.d("developer",coreV1.getJac())
     }
 }
